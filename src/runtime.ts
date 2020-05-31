6d27fbaeab1def0c2db6704e874cea698a5ae7c5
Brian LeRoux <b@brian.io>
2020-05-30 18:04:26 -0700

    distill into single responsibility pure functions as much as possible

--- src/runtime.ts
+++ src/runtime.ts
@@ -3,10 +3,12 @@ let env = Deno.env.toObject();
 let api = env.AWS_LAMBDA_RUNTIME_API;
 let name = env._HANDLER.split(".")[0];
 let method = env._HANDLER.split(".")[1];
+let path = `${env.LAMBDA_TASK_ROOT}/${name}.ts`;
+let invoke = `http://${api}/2018-06-01/runtime/invocation`;
+let error = `http://${api}/2018-06-01/runtime/init/error`;
 
 // get the handler entry file
 let handler;
-let path = `${env.LAMBDA_TASK_ROOT}/${name}.ts`;
 let found = await exists(path);
 if (found) {
   let mod = await import(path);
@@ -16,43 +18,47 @@ if (found) {
 
 // if entry file is missing or invalid bail hard with a meaningful error
 if (!found) {
-  await post(`http://${api}/2018-06-01/runtime/init/error`, {
+  await post(error, {
     errorType: "HandlerNotFound",
     errorMessage: `expected "${path}" to export a function named "${method}"`,
   });
   Deno.exit(1);
 }
 
-// start the event loop
+// all good! start the event loop
 while (true) {
-  // massage invocation payload
-  let invoke = `http://${api}/2018-06-01/runtime/invocation`;
-  let next = await fetch(`${invoke}/next`);
-  let event = await next.json();
-  let reqID = next.headers.get("Lambda-Runtime-Aws-Request-Id");
+  let { event, context } = await next(`${invoke}/next`);
+  try {
+    // invoke the handler
+    const payload = await handler(event, context);
+    await post(`${invoke}/${context.awsRequestId}/response`, payload);
+  } catch (err) {
+    // ensure errors bubble
+    await post(`${invoke}/${context.awsRequestId}/error`, {
+      errorType: err.name,
+      errorMessage: err.message,
+      stackTrace: err.stack,
+    });
+  }
+}
+
+/** helper to get next invocation */
+async function next(url: string) {
+  let env = Deno.env.toObject();
+  let req = await fetch(url);
+  let reqID = req.headers.get("Lambda-Runtime-Aws-Request-Id");
+  let arn = req.headers.get("lambda-runtime-invoked-function-arn");
+  let event = await req.json();
   let context = {
-    awsRequestId: next.headers.get("lambda-runtime-aws-request-id"),
-    invokedFunctionArn: next.headers.get("lambda-runtime-invoked-function-arn"),
+    awsRequestId: reqID,
+    invokedFunctionArn: arn,
     logGroupName: env.AWS_LAMBDA_LOG_GROUP_NAME,
     logStreamName: env.AWS_LAMBDA_LOG_STREAM_NAME,
     functionName: env.AWS_LAMBDA_FUNCTION_NAME,
     functionVersion: env.AWS_LAMBDA_FUNCTION_VERSION,
     memoryLimitInMB: env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE,
   };
-
-  // invoke the handler
-  try {
-    let res = await handler(event, context);
-    await post(`${invoke}/${reqID}/response`, res);
-  } catch (err) {
-    await post(`${invoke}/${reqID}/error`, {
-      errorType: err.name,
-      errorMessage: err.message,
-      stackTrace: err.stack,
-    });
-  }
-
-  // end while
+  return { event, context };
 }
 
 /** helper to post message to lambda */
@@ -68,14 +74,11 @@ async function post(url: string, payload: object) {
 async function exists(filename: string): Promise<boolean> {
   try {
     await Deno.stat(filename);
-    // successful, file or directory must exist
     return true;
   } catch (error) {
     if (error instanceof Deno.errors.NotFound) {
-      // file or directory does not exist
       return false;
     } else {
-      // unexpected error, maybe permissions, pass it along
       throw error;
     }
   }
