761aed6cc3e5a5735f51c37424e801cb00a685de
Brian LeRoux <b@brian.io>
2022-02-28 14:36:48 -0800

    hbd workflo

--- fix.sh
+++ fix.sh
@@ -1,3 +1,4 @@
 aws lambda add-layer-version-permission --layer-name DenoRuntime \
---statement-id allow-everyone1 --version-number 6 --principal '*' \
+--statement-id allow-everyone1 --version-number 11 --principal '*' \
 --action lambda:GetLayerVersion
+
