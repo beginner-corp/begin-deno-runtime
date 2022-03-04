88e4efc87d03780aedcba63ce34f4c76361016a4
Brian LeRoux <b@brian.io>
2022-03-04 12:20:26 -0800

    adds some logging to see wtf is up

--- scripts/publish-layers.js
+++ scripts/publish-layers.js
@@ -11,28 +11,33 @@ let regions = require('./supported-regions')
 })();
 
 async function publish (region) {
-
-  let lambda = new aws.Lambda({ region })
-
-  // read the zip
-  let pathToFile = path.join(__dirname, '..', `deno-${process.env.version}-x86.zip`)
-  let file = fs.readFileSync(pathToFile).toString('base64')
-
-  // publish the zip
-  let { Version } = await lambda.publishLayerVersion({
-    LayerName: `DenoRuntime`,
-    Description: `${ process.env.version }-x86`,
-    CompatibleArchitectures: ['x86_64'],
-    Content: { ZipFile: file },
-    LicenseInfo: 'Apache-2.0' 
-  }).promise()
-
-  // reset the permissions
-  return lambda.addLayerVersionPermission({
-    Action: 'lambda:GetLayerVersion',
-    LayerName: `DenoRuntime`,
-    Principal: '*',
-    StatementId: `allow-${ Date.now() }`,
-    VersionNumber: Version,
-  }).promise()
+  console.log('publish to layer to', region)
+  try {
+    let lambda = new aws.Lambda({ region })
+ 
+    // read the zip
+    let pathToFile = path.join(__dirname, '..', `deno-${process.env.version}-x86.zip`)
+    let file = fs.readFileSync(pathToFile).toString('base64')
+ 
+    // publish the zip
+    let { Version } = await lambda.publishLayerVersion({
+      LayerName: `DenoRuntime`,
+      Description: `${ process.env.version }-x86`,
+      CompatibleArchitectures: ['x86_64'],
+      Content: { ZipFile: file },
+      LicenseInfo: 'Apache-2.0' 
+    }).promise()
+ 
+    // reset the permissions
+    return lambda.addLayerVersionPermission({
+      Action: 'lambda:GetLayerVersion',
+      LayerName: `DenoRuntime`,
+      Principal: '*',
+      StatementId: `allow-${ Date.now() }`,
+      VersionNumber: Version,
+    }).promise()
+  }
+  catch (e) {
+    return e
+  }
 }
